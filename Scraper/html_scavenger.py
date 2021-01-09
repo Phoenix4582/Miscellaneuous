b4db14d2ba3cd2590456aa3e16f502c633ff1285
Phoenix4582 <56557761+Phoenix4582@users.noreply.github.com>
2021-01-09 10:41:52 +0800

    Html_scraper

---
+++ Scraper/html_scavenger.py
@@ -0,0 +1,43 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+url = 'https://www.newegg.com/p/pl?d=graphic+card&cm_sp=KeywordRelated-_-graphic%20cards-_-graphic%20card-_-INFOCARD'
+
+# opening up connection, grabbing the page
+uClient = uReq(url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, 'html.parser')
+
+# grab each product
+containers = page_soup.findAll("div", {"class": "item-cell"})
+
+filename = "products.csv"
+f = open(filename, "w+")
+
+headers = "brand,product_name,shipping\n"
+f.write(headers)
+# get specific element
+for container in containers:
+    try:
+        brand = container.div.div.a.img["title"]
+
+        title_container = container.findAll("a", {"class": "item-title"})
+        product_name = title_container[0].text
+
+        shipping_container = container.findAll('li', {"class": "price-ship"})
+        shipping = shipping_container[0].text.strip()
+
+        print("brand " + brand)
+        print("product_name " + product_name)
+        print("shipping " + shipping)
+
+        f.write(brand + "," + product_name.replace(",","|") + "," + shipping + "\n")
+
+    except:
+        continue
+
+f.close()
