94724148833331c22c95ba901857f4ee51f643e9
Phoenix4582 <56557761+Phoenix4582@users.noreply.github.com>
2021-06-11 21:55:08 +0800

    Add files via upload

---
+++ SpeedTest/internet_speed.py
@@ -0,0 +1,23 @@
+import speedtest
+
+def main():
+    test = speedtest.Speedtest()
+
+    print("Loading server list......")
+    test.get_servers() # -> get list of servers
+    print("Choosing best server......")
+    best = test.get_best_server() # -> choose best server
+
+    print(f"Found : {best['host']} located in {best['country']}")
+
+    # Speed testings on various utilities
+    print("Perform download test......")
+    download_result = test.download()
+    print("Perform upload test......")
+    upload_result = test.upload()
+    print("Perform ping test......")
+    ping_result = test.results.ping
+
+    print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
+    print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
+    print(f"Ping: {ping_result:.2f} ms")
